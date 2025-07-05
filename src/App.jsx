5390c926517fc6ae86ae77e9974680d384fe7bd7
Chirag Jain <jainc451@gmail.com>
2025-07-05 17:45:09 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Pokedex from './Components/Pokedex/Pokedex';
+import CustomRoutes from './Routes/CustomRoutes';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Pokedex />
-    </>
+    <div className="outer-pokedex">
+      <h1 id="pokedex-heading">
+        <Link to="/"> Pokedex </Link>
+      </h1>
+      <CustomRoutes />
+    </div>
   );
 }
 
--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,12 +1,15 @@
 import './Pokemon.css'
-
-function Pokemon({name,image}){
+import { Link } from 'react-router-dom'
+function Pokemon({name,image,id}){
     return (
         <div className='pokemon'>
-            <div className='pokemon-name'>{name}</div>
-            <div ><img className='pokemon-image'src={image}/></div>
+            <Link to={`/pokemon/${id}`}>
+            {/*<a href="/pokemon/2" />  but problem is that new page refreshes everytime*/} 
+                <div className='pokemon-name'>{name}</div>
+                <div ><img className='pokemon-image'src={image}/></div>
+            </Link>
         </div>
-    )
+    );
 }
 
 export default Pokemon;
